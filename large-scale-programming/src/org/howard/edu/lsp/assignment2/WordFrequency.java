d258e45152cf6d9fe89287f7b14f0600adafa379
iamheretodevelop <hrishavsapkota1@gmail.com>
2024-02-06 12:19:10 -0500

    progress so far today

--- large-scale-programming/src/org/howard/edu/lsp/assignment2/WordFrequency.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment2/WordFrequency.java
@@ -1,10 +1,39 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+
 public class WordFrequency {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String wordsFilePath = "/src/org/howard/edu/lsp/assignment2.words.txt";
+		
+		String currDirectory = System.getProperty("user.dir");
+		Path filePath = Paths.get(currDirectory, wordsFilePath);
+		
+		try {
+			String fileWords = new String(Files.readAllBytes(filePath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LinkedHashMap<String, Integer> wordMap = new LinkedHashMap<>;
+		StringBuilder var = new StringBuilder();
+		
+		String[] arr = fileWords.split("\\s+");
 	}
+	
+	private static boolean checkWord(String word) {
+		for (String letter : word) {
+			if letter.isalpha()
+		}
+		return true;
+		
+		return false;
+	}
+		
 
 }
