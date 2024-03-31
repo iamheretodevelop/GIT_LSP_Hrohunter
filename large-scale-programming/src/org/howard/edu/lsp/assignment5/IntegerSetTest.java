cf40199b014566a708f1a79640c7406e6bc90e75
iamheretodevelop <hrishavsapkota1@gmail.com>
2024-03-31 16:27:16 -0400

    Changed import

--- large-scale-programming/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -1,6 +1,6 @@
 package org.howard.edu.lsp.assignment5;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
